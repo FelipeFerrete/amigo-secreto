b1b30038b0e0663dfadb3228d27af3eba70e4fae
FelipeFerrete <felipeferretelemes@gmail.com>
2025-04-07 17:30:08 -0300

    Funcion Embaralha

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
+let amigos = [];
+
 function adicionar(){
     let amigo = document.getElementById("nome-amigo");
     let lista = document.getElementById("lista-amigos");
+    amigos.push(amigo.value)
     if(lista.textContent == "") {
         lista.textContent = amigo.value;
     } else {
         lista.textContent = lista.textContent + ', ' + amigo.value;
     }
     amigo.value = ""; // Limpa o campo de texto após adicionar
+}
+
+function sortear() {
+
+}
+
+function embaralhar() {
+    for (let indice = lista.length; indice;indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        [lista[indice - 1], lista[indiceAleatorio]] =
+         [lista[indiceAleatorio], lista[indice - 1]];
+ }
 }
